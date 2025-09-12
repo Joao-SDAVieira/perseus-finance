346406e487f2b695e5570d00980db429667adc8d
Joao_SDAVieira <eujoaovictor0707@gmail.com>
2025-09-11 21:20:41 -0300

    Criando projeto spring!

---
+++ src/test/java/com/perseusfinance/perseusfinance/PerseusFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perseusfinance.perseusfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerseusFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
